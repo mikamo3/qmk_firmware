2b1b3912da6ef2988bde649d6e64ab5fc0f50a0c
mikamo3 <kamo3proj@gmail.com>
2021-01-27 03:57:29 +0900

    :children_crossing: (manaita) fix toggle jp layout key

--- keyboards/mikamo3/manaita/keymaps/default/keymap.c
+++ keyboards/mikamo3/manaita/keymaps/default/keymap.c
@@ -28,7 +28,7 @@ enum layer_names {
     _RAISE_JP
 
 };
-enum { TD_LCTLGUI = 0, TD_RALTGUI = 1 };
+enum { TD_LCTLGUI = 0, TD_RALTGUI = 1,TD_RSFTJP =2 };
 enum manaita_keycodes {
     KC_LOWER=SAFE_RANGE,
     KC_RAISE,
@@ -42,29 +42,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE]     = LAYOUT(
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_BSPC,
         LCTL_T(KC_TAB),KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
-         KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH), _______,
+         KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(TD_RSFTJP),
         TD(TD_LCTLGUI), LT(_LOWER, KC_SPC), LT(_RAISE, KC_ENT), TD(TD_RALTGUI)),
     [_LOWER]    = LAYOUT(
         KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_QUOT, KC_DEL,
         C(KC_SPC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, _______,
         _______, KC_EQL, KC_LT, KC_LCBR, KC_LBRC, KC_MINS, KC_RBRC, KC_RCBR, KC_GT, _______, _______, _______,
-         _______, _______, TO(_BASE_JP), _______),
+         _______, _______, _______, _______),
     [_RAISE]    = LAYOUT(_______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
     C(KC_SPC), KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
      _______, KC_PSCR, KC_INS, KC_SLCK, KC_PAUS, _______, _______, _______, _______, _______, _______, _______,
-      _______, TO(_BASE_JP), _______, _______),
+      _______, _______, _______, _______),
     [_BASE_JP]  = LAYOUT(KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, JP_YEN, KC_BSPC,
      LCTL_T(KC_TAB),KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, JU_SCLN, KC_ENT,
-      KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(JP_SLSH), _______,
+      KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, JP_SLSH, TD(TD_RSFTJP),
     TD(TD_LCTLGUI), LT(_LOWER_JP, KC_SPC), LT(_RAISE_JP, KC_ENT), TD(TD_RALTGUI)),
     [_LOWER_JP] = LAYOUT(JU_GRV, JP_EXLM, JP_AT, JP_HASH, JP_DLR, JP_PERC, JP_CIRC, JP_AMPR, JP_ASTR, JP_LPRN, JP_RPRN, JU_QUOT, KC_DEL,
      JP_ZKHK,KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, _______, _______,
       JU_EQL, JP_LT, JP_LCBR, JP_LBRC, JU_MINS, JP_RBRC, JP_RCBR, KC_GT, _______, _______, _______,
-      _______, _______, TO(_BASE), _______),
+      _______, _______, _______, _______),
     [_RAISE_JP] = LAYOUT(_______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
      JP_ZKHK, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
       _______, KC_PSCR, KC_INS, KC_SLCK, KC_PAUS, _______, _______, _______, _______, _______, _______, _______,
-       _______, TO(_BASE), _______, _______)};
+       _______, _______, _______, _______)};
 
 typedef struct {
     bool    is_press_action;
@@ -75,11 +75,14 @@ enum { SINGLE = 1, DOUBLE, TRIPLE, QUAD };
 uint8_t    cur_dance(qk_tap_dance_state_t *state);
 void       lgui_finished(qk_tap_dance_state_t *state, void *user_data);
 void       rgui_finished(qk_tap_dance_state_t *state, void *user_data);
+void       rsft_finished(qk_tap_dance_state_t *state, void *user_data);
 void       lgui_reset(qk_tap_dance_state_t *state, void *user_data);
 void       rgui_reset(qk_tap_dance_state_t *state, void *user_data);
+void       rsft_reset(qk_tap_dance_state_t *state, void *user_data);
 
 static tap lgui_tap_state = {.is_press_action = true, .state = 0};
 static tap rgui_tap_state = {.is_press_action = true, .state = 0};
+static tap rsft_tap_state = {.is_press_action = true, .state = 0};
 
 uint8_t    cur_dance(qk_tap_dance_state_t *state) {
     if (state->count == 1) {
@@ -135,6 +138,24 @@ void rgui_finished(qk_tap_dance_state_t *state, void *user_data) {
     }
 }
 
+
+void rsft_finished(qk_tap_dance_state_t *state, void *user_data) {
+    rsft_tap_state.state = cur_dance(state);
+    switch (rsft_tap_state.state) {
+        case SINGLE:
+            register_code(KC_RSFT);
+            break;
+        case DOUBLE:
+            if(IS_LAYER_ON(_BASE)){
+                layer_on(_BASE_JP);
+                layer_off(_BASE);
+            }else{
+                layer_on(_BASE);
+                layer_off(_BASE_JP);
+            }
+            break;
+    }
+}
 void lgui_reset(qk_tap_dance_state_t *state, void *user_data) {
     switch (lgui_tap_state.state) {
         case SINGLE:
@@ -175,9 +196,19 @@ void rgui_reset(qk_tap_dance_state_t *state, void *user_data) {
     rgui_tap_state.state = 0;
 }
 
+
+void rsft_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (rsft_tap_state.state) {
+        case SINGLE:
+            unregister_code(KC_RSFT);
+            break;
+    }
+    rsft_tap_state.state = 0;
+}
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_LCTLGUI] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, lgui_finished, lgui_reset),
-    [TD_RALTGUI] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, rgui_finished, rgui_reset)};
+    [TD_RALTGUI] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, rgui_finished, rgui_reset),
+    [TD_RSFTJP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, rsft_finished, rsft_reset)};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     bool continue_process = process_record_user_jtu(keycode, record);
