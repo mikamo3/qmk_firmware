551bd09a8b7b97797b0006b3ae6e73f1c8b217ec
mikamo3 <kamo3proj@gmail.com>
2021-01-28 02:32:21 +0900

    :sparkles: (kaplus) add custom keycode

--- keyboards/mikamo3/kaplus/keymaps/default/keymap.c
+++ keyboards/mikamo3/kaplus/keymaps/default/keymap.c
@@ -41,20 +41,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,KC_MINS,KC_EQL, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         KC_TAB,KC_A, KC_S, KC_D, KC_F, KC_G,KC_LBRC,KC_RBRC, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_BSLS,
     KC_LCTL,  KC_Z, KC_X, KC_C, KC_V, KC_B,               KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-            KC_LSFT,C(KC_SPC),TD(TD_LGUI),LT(_LOWER,KC_SPC),LCTL_T(KC_ESC),LSFT_T(KC_BSPC),LT(_RAISE,KC_ENT),TD(TD_RGUI),KC_RALT,TD(TD_RSFTJP)
+            KC_LSFT,C(KC_SPC),TD(TD_LGUI),KC_LOWER,LCTL_T(KC_ESC),LSFT_T(KC_BSPC),KC_RAISE,TD(TD_RGUI),KC_RALT,TD(TD_RSFTJP)
 
     ),
     [_LOWER] = LAYOUT(
         _______,KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,KC_DQUO,KC_QUOT,KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,KC_DEL,
         _______, KC_1, KC_2, KC_3, KC_4, KC_5,KC_LCBR,KC_RCBR, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV,
     _______,  _______, _______, KC_TILD, KC_PLUS, KC_MINS,               KC_ASTR, KC_SLSH, KC_GT, KC_LT, KC_QUES, _______,
-    _______,_______,_______,_______,_______,LSFT_T(KC_DEL),TO(_BASE_JP),_______,_______,_______
+    _______,_______,_______,_______,_______,LSFT_T(KC_DEL),_______,_______,_______,_______
     ),
     [_RAISE] = LAYOUT(
         _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,KC_F11,KC_F12,KC_F6,KC_F7, KC_F8, KC_F9, KC_F10,  _______,
 _______,_______,KC_END,KC_PGDN,KC_PGUP,KC_HOME,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,_______,_______,
 _______,_______,KC_PSCR,KC_INS,KC_SLCK,KC_PAUS,_______,_______,_______,_______,_______,_______,
-    _______,_______,_______,TO(_BASE_JP),_______,LSFT_T(KC_DEL),_______,_______,_______,_______
+    _______,_______,_______,_______,_______,LSFT_T(KC_DEL),_______,_______,_______,_______
 
 
     ),
@@ -62,20 +62,20 @@ _______,_______,KC_PSCR,KC_INS,KC_SLCK,KC_PAUS,_______,_______,_______,_______,_
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,JU_MINS,JU_EQL, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
          KC_TAB,KC_A, KC_S, KC_D, KC_F, KC_G,JP_LBRC,JP_RBRC, KC_H, KC_J, KC_K, KC_L, JU_SCLN, JP_YEN,
     KC_LCTL,  KC_Z, KC_X, KC_C, KC_V, KC_B,               KC_N, KC_M, KC_COMM, KC_DOT, JP_SLSH, KC_RSFT,
-            KC_LSFT,JP_ZKHK,TD(TD_LGUI),LT(_LOWER_JP,KC_SPC),LCTL_T(KC_ESC),LSFT_T(KC_BSPC),LT(_RAISE_JP,KC_ENT),TD(TD_RGUI),KC_RALT,TD(TD_RSFTJP)
+            KC_LSFT,JP_ZKHK,TD(TD_LGUI),KC_LOWER_JP,LCTL_T(KC_ESC),LSFT_T(KC_BSPC),KC_RAISE_JP,TD(TD_RGUI),KC_RALT,TD(TD_RSFTJP)
 
     ),
     [_LOWER_JP] = LAYOUT(
         _______,JP_EXLM, JP_AT, JP_HASH, JP_DLR, JP_PERC,JP_DQUO,JU_QUOT,JP_CIRC, JP_AMPR, JP_ASTR, JP_LPRN, JP_RPRN,KC_DEL,
         _______, KC_1, KC_2, KC_3, KC_4, KC_5,JP_LCBR,JP_RCBR, KC_6, KC_7, KC_8, KC_9, KC_0, JU_GRV,
     _______,  _______, _______, JU_GRV, JP_PLUS, JU_MINS,               JP_ASTR, JP_SLSH, KC_GT, KC_LT, KC_QUES, _______,
-    _______,_______,_______,_______,_______,LSFT_T(KC_DEL),TO(_BASE),_______,_______,_______
+    _______,_______,_______,_______,_______,LSFT_T(KC_DEL),_______,_______,_______,_______
     ),
     [_RAISE_JP] = LAYOUT(
         _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,KC_F11,KC_F12,KC_F6,KC_F7, KC_F8, KC_F9, KC_F10,  _______,
 _______,_______,KC_END,KC_PGDN,KC_PGUP,KC_HOME,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,_______,_______,
 _______,_______,KC_PSCR,KC_INS,KC_SLCK,KC_PAUS,_______,_______,_______,_______,_______,_______,
-    _______,_______,_______,TO(_BASE),_______,LSFT_T(KC_DEL),_______,_______,_______,_______
+    _______,_______,_______,_______,_______,LSFT_T(KC_DEL),_______,_______,_______,_______
 
 
     ),
@@ -213,24 +213,85 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    bool continue_process = process_record_user_jtu(keycode, record);
+    static uint32_t key_timer;
+    static bool     lower_pressed    = false;
+    static bool     raise_pressed    = false;
+    static bool     lower_jp_pressed = false;
+    static bool     raise_jp_pressed = false;
+    bool            continue_process = process_record_user_jtu(keycode, record);
+
     if (continue_process == false) {
         return false;
     }
-    switch (keycode) {}
+    switch (keycode) {
+        case KC_LOWER:
+            if (record->event.pressed) {
+                key_timer = timer_read32();
+                layer_on(_LOWER);
+                lower_pressed = true;
+            } else {
+                if (lower_pressed && timer_elapsed32(key_timer) <= TAPPING_TERM) {
+                    register_code(KC_SPC);
+                    unregister_code(KC_SPC);
+                }
+                layer_off(_LOWER);
+                lower_pressed = false;
+            }
+            return false;
+            break;
+        case KC_RAISE:
+            if (record->event.pressed) {
+                key_timer = timer_read32();
+                layer_on(_RAISE);
+                raise_pressed = true;
+            } else {
+                if (raise_pressed && timer_elapsed32(key_timer) <= TAPPING_TERM) {
+                    register_code(KC_ENT);
+                    unregister_code(KC_ENT);
+                }
+                layer_off(_RAISE);
+                raise_pressed = false;
+            }
+            return false;
+            break;
+        case KC_LOWER_JP:
+            if (record->event.pressed) {
+                key_timer = timer_read32();
+                layer_on(_LOWER_JP);
+                lower_jp_pressed = true;
+            } else {
+                if (lower_jp_pressed && timer_elapsed32(key_timer) <= TAPPING_TERM) {
+                    register_code(KC_SPC);
+                    unregister_code(KC_SPC);
+                }
+                layer_off(_LOWER_JP);
+                lower_jp_pressed = false;
+            }
+            return false;
+            break;
+        case KC_RAISE_JP:
+            if (record->event.pressed) {
+                key_timer = timer_read32();
+                layer_on(_RAISE_JP);
+                raise_jp_pressed = true;
+            } else {
+                if (raise_jp_pressed && timer_elapsed32(key_timer) <= TAPPING_TERM) {
+                    register_code(KC_ENT);
+                    unregister_code(KC_ENT);
+                }
+                layer_off(_RAISE_JP);
+                raise_jp_pressed = false;
+            }
+            return false;
+            break;
+        default:
+            if (record->event.pressed) {
+                lower_pressed    = false;
+                raise_pressed    = false;
+                lower_jp_pressed = false;
+                raise_jp_pressed = false;
+            }
+            break;
+    }
     return true;
 }
-
-/*
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-bool led_update_user(led_t led_state) {
-    return true;
-}
-*/
